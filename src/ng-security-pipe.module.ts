69645810092f9eaf8e805b5e373f47e2e3052faa
Porawit Poboonma <ball6847@gmail.com>
2018-01-12 04:09:11 +0700

    apply prettier

--- src/ng-security-pipe.module.ts
+++ src/ng-security-pipe.module.ts
@@ -4,14 +4,8 @@ import { NgModule } from '@angular/core'
 import { SanitizeHtmlPipe } from './sanitize-html/sanitize-html.pipe'
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [
-    SanitizeHtmlPipe
-  ],
-  exports: [
-    SanitizeHtmlPipe
-  ]
+  imports: [CommonModule],
+  declarations: [SanitizeHtmlPipe],
+  exports: [SanitizeHtmlPipe]
 })
-export class NgSecurityPipeModule { }
+export class NgSecurityPipeModule {}
